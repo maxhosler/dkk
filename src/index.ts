fe659c3680cadba9226f6c1ac940f8a3f59a6528
Max Hosler <mhoslersse@gmail.com>
2025-02-05 13:55:26 -0500

    window start

--- src/index.ts
+++ src/index.ts
@@ -1,25 +1,66 @@
 import { DrawOptions } from "./subelements/dag_canvas";
 import { prebuilt_dag_embedding } from "./dag_layout";
 import { CliqueViewer } from "./modes/clique_viewer";
+import { EmbeddingEditor } from "./modes/embedding_editor";
 
+abstract class Popup
+{
+    base: HTMLDivElement;
+    window: HTMLDivElement;
+    constructor(body: HTMLElement)
+    {
+        let base = document.createElement("div");
+        base.id = "shadow"
+        base.className = "fullscreen"
+        this.base = base;
+        body.appendChild(base);
+
+        let window = document.createElement("div");
+        window.className = "popup-window";
+        base.appendChild(window);
+        this.window = window;
+    }
+
+    close()
+    {
+        this.base.remove();
+    }
+}
+
+class OpenPopup extends Popup
+{
+    constructor(base: HTMLElement)
+    {
+        super(base);
+    }
+}
 
 class DKKProgram
 {
+    body: HTMLBodyElement;
 	draw_options = new DrawOptions();
-	current_clique: number = 0;
-	mode: CliqueViewer = CliqueViewer.destructive_new(
+	mode: CliqueViewer | EmbeddingEditor = CliqueViewer.destructive_new(
 		prebuilt_dag_embedding(0),
 		this.draw_options
 	);
+    popup_open: boolean = false;
 
 	constructor()
 	{
+        this.body = document.getElementsByTagName("body")[0] as HTMLBodyElement;
 		let open_button: HTMLDivElement = document.getElementById("open-button") as HTMLDivElement;
 		open_button.onclick = (ev) => {
-			this.current_clique = (this.current_clique+1)%3;
-			this.set_clique_viewer(this.current_clique);
+            this.open_button_click();
 		};
 	}
+
+    open_button_click()
+    {
+        if(this.popup_open) { return; }
+
+        this.popup_open = true;
+        let popup = new OpenPopup(this.body);
+    }
 
 	set_clique_viewer(idx: number)
 	{
