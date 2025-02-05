4fe40c4a5801ac6a78848fb0865ab023c08780c3
Max Hosler <mhoslersse@gmail.com>
2025-02-04 23:01:22 -0500

    menu swapping

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,31 @@
 import { DrawOptions } from "./subelements/dag_canvas";
 import { prebuilt_dag_embedding } from "./dag_layout";
 import { CliqueViewer } from "./modes/clique_viewer";
-import { EmbeddingEditor } from "./modes/embedding_editor";
-import { DAGCliques } from "./routes/routes";
 
-const draw_options = new DrawOptions();
-const layout = prebuilt_dag_embedding(2);
-const pm = CliqueViewer.destructive_new(layout, draw_options);
+
+class DKKProgram
+{
+	draw_options = new DrawOptions();
+	current_clique: number = 0;
+	mode: CliqueViewer = CliqueViewer.destructive_new(
+		prebuilt_dag_embedding(0),
+		this.draw_options
+	);
+
+	constructor()
+	{
+		let open_button: HTMLDivElement = document.getElementById("open-button") as HTMLDivElement;
+		open_button.onclick = (ev) => {
+			this.current_clique = (this.current_clique+1)%3;
+			this.set_clique_viewer(this.current_clique);
+		};
+	}
+
+	set_clique_viewer(idx: number)
+	{
+		var layout = prebuilt_dag_embedding(idx);
+		this.mode = CliqueViewer.destructive_new(layout, this.draw_options);
+	}
+}
+
+let dkk = new DKKProgram();
